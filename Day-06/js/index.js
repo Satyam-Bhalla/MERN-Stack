77e01102bd260761195097aebe5bfce93d4a1f8b
Satyam-Bhalla <satyambhalla17@gmail.com>
2021-05-31 10:01:47 +0530

    Javascript 1

---
+++ Day-06/js/index.js
@@ -0,0 +1,25 @@
+
+// function square(num){
+//     var a = 10;
+//     console.log(a);
+//     return num*num;
+// }
+
+/*
+    This is a multiline comment
+*/
+
+// console.log("External file");
+// document.getElementById("heading").innerHTML = "I am changed";
+
+var a = "Hello";
+var b = 20;
+var c = 10.5;
+var d = ["hello","world"];
+var e = ["hello", 1];
+
+console.log(typeof (2+a));
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
