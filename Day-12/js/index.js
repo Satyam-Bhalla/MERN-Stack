eb5de7aef2f967ed5a1e64065abefb13076e4e95
Satyam-Bhalla <satyambhalla17@gmail.com>
2021-06-07 10:14:10 +0530

    Day 12

---
+++ Day-12/js/index.js
@@ -0,0 +1,135 @@
+// Error handling
+// try, catch and finally
+// Error Creation -> throw
+
+
+function errorHandling(flag){
+    try{
+        let a = 10;
+        if(flag && isNaN("sdfkhb")){
+            throw {message:"Invalid statement", code: 1001};
+        }else{
+            console.log("No error");
+        }
+    }catch(err){
+        console.log(typeof err);
+        console.log(err.message);
+        console.log(err.code);
+    }finally{
+        console.log("Clear memory");
+    }
+}
+
+// errorHandling(true);
+
+
+function change(obj){
+    obj.innerHTML = "Text changed";
+    console.log("Text is going to change");
+}
+
+function showPopUp(){
+    // alert("Welcome to the website");
+    // console.log("Website loaded");
+}
+
+
+function changeBackground(obj){
+    obj.style.backgroundColor = "red";
+}
+
+function changeBackgroundAgain(obj){
+    obj.style.backgroundColor = "white";
+}
+
+function changeCase(){
+    let element = document.getElementById("name");
+    element.value = element.value.toUpperCase();
+}
+
+// document.getElementById("name").addEventListener('focus',function(){
+//     document.getElementById("name").style.borderColor = "blue";
+//     document.getElementById("name").style.borderWidth = "2px";
+// })
+
+// IIFE -> Immediately invoked function expression
+(function(){
+    // document.getElementById("para").onclick = change;
+})();
+
+// alert()
+
+
+// Callbacks
+// function first(){
+//     // document.getElementById("para").innerHTML = "Hello";
+//     setTimeout(function(){
+//         console.log("First function is running");
+//     },200);
+
+//     return 0;
+// }
+
+// function second(first){
+//     let value = first();
+//     document.getElementById("para").innerHTML = value;
+// }
+
+// // first();
+// second(first);
+
+// const message =  function(){
+//     console.log("This is a  message");
+// }
+// // message();
+// setTimeout(message, 1000);
+// console.log("Hello");
+// for(let i = 0;i<100000;i++){
+
+// }
+// console.log("Loop finished!");
+
+
+// Callback hell
+/**
+ * firstRequest(function(response){
+     secondRequest(response, function(nextResponse){
+        thirdRequest(nextResponse, function(finalResponse){
+            console.log(finalResponse)
+        },errorCheck)
+    },errorCheck)
+ }, errorCheck)
+  
+ */
+
+
+
+
+
+// Promises
+/*
+    1.) Pending
+    2.) Fullfilled / Resolved
+    3.) Rejected
+
+    Chaining is done using then and catch functions
+    */ 
+
+// firstRequest()
+// .then(function(response){
+//     return secondRequest(response);
+// }).then(fucntion(nextResponse){
+//     return thirdRequest(nextResponse);
+// }).then(function(finalResponse){
+//     console.log(finalResponse);
+// }).catch(failureCallback){
+
+// }
+const myPromise = new Promise(function(resolve,reject) {
+    // if condition mets then resolve otherwise reject
+    if(true){
+        resolve("Promise resolved");
+    }else{
+        reject("Promise rejected");
+    }
+})
