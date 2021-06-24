28fa80ef9b83e7c32bb03d71821189fec9e1f952
Satyam-Bhalla <satyambhalla17@gmail.com>
2021-06-24 09:56:30 +0530

    Deployment

---
+++ Utilities/fileUpload.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+const uuid = require('uuid/v1');
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
+const fileUpload = multer({
+  limits: 5000000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/images');
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
+    }
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type!');
+    cb(error, isValid);
+  }
+});
+
+module.exports = fileUpload;
